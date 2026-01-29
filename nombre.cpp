a6cc292399a74c85bcfe8fb374fa28a2daaf577f
JosuéMuy <josuemuy.m@gmail.com>
2026-01-28 21:18:28 -0600

    mostrar datos personales

---
+++ nombre.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    cout << "Freddy Josue Muy Morales" << endl;
+    cout << "9959-25-4992" << endl;
+    return 0;
+}
